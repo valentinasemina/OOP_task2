059a38439b853b7077b814689fada516cbe78cbe
valentinasemina <70881558+valentinasemina@users.noreply.github.com>
2020-09-11 23:37:06 +0300

    Added seminar1_test

---
+++ Semina_seminar1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btplus = new System.Windows.Forms.Button();
+            this.btminus = new System.Windows.Forms.Button();
+            this.btdel = new System.Windows.Forms.Button();
+            this.btumn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 23);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(104, 49);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(42, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(188, 39);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(88, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // btplus
+            // 
+            this.btplus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btplus.Location = new System.Drawing.Point(18, 96);
+            this.btplus.Name = "btplus";
+            this.btplus.Size = new System.Drawing.Size(38, 30);
+            this.btplus.TabIndex = 7;
+            this.btplus.Text = "+";
+            this.btplus.UseVisualStyleBackColor = true;
+            this.btplus.Click += new System.EventHandler(this.btplus_Click);
+            // 
+            // btminus
+            // 
+            this.btminus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btminus.Location = new System.Drawing.Point(66, 96);
+            this.btminus.Name = "btminus";
+            this.btminus.Size = new System.Drawing.Size(38, 30);
+            this.btminus.TabIndex = 8;
+            this.btminus.Text = "-";
+            this.btminus.UseVisualStyleBackColor = true;
+            this.btminus.Click += new System.EventHandler(this.btminus_Click);
+            // 
+            // btdel
+            // 
+            this.btdel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btdel.Location = new System.Drawing.Point(18, 132);
+            this.btdel.Name = "btdel";
+            this.btdel.Size = new System.Drawing.Size(38, 30);
+            this.btdel.TabIndex = 9;
+            this.btdel.Text = "/";
+            this.btdel.UseVisualStyleBackColor = true;
+            this.btdel.Click += new System.EventHandler(this.btdel_Click);
+            // 
+            // btumn
+            // 
+            this.btumn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btumn.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btumn.Location = new System.Drawing.Point(66, 132);
+            this.btumn.Name = "btumn";
+            this.btumn.Size = new System.Drawing.Size(38, 30);
+            this.btumn.TabIndex = 10;
+            this.btumn.Text = "*";
+            this.btumn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btumn.UseVisualStyleBackColor = true;
+            this.btumn.Click += new System.EventHandler(this.btumn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "=";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(120, 96);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(38, 30);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 226);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btumn);
+            this.Controls.Add(this.btdel);
+            this.Controls.Add(this.btminus);
+            this.Controls.Add(this.btplus);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btplus;
+        private System.Windows.Forms.Button btminus;
+        private System.Windows.Forms.Button btdel;
+        private System.Windows.Forms.Button btumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Semina_seminar1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static Rational ReadX(TextBox tb1, TextBox tb2)
+        {
+            int up = Convert.ToInt32(tb1.Text);
+            int down = Convert.ToInt32(tb2.Text);
+            Rational x = new Rational(up,down);
+            return x;
+        }
+
+        static Rational ReadY(TextBox tb1, TextBox tb2)
+        {
+            int up = Convert.ToInt32(tb1.Text);
+            int down = Convert.ToInt32(tb2.Text);
+            Rational y = new Rational(up, down);
+            return y;
+        }
+
+        private void btplus_Click(object sender, EventArgs e)
+        {
+            Rational z = ReadX(textBox1,textBox2)+(ReadY(textBox3,textBox4));
+            textBox5.Text = z.Tostring(z);
+        }
+
+        private void btminus_Click(object sender, EventArgs e)
+        {
+            Rational z = ReadX(textBox1, textBox2)-(ReadY(textBox3, textBox4));
+            textBox5.Text = z.Tostring(z);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void btumn_Click(object sender, EventArgs e)
+        {
+            Rational z = ReadX(textBox1, textBox2)*(ReadY(textBox3, textBox4));
+            textBox5.Text = z.Tostring(z);
+        }
+
+        private void btdel_Click(object sender, EventArgs e)
+        {
+            Rational z = ReadX(textBox1, textBox2)/(ReadY(textBox3, textBox4));
+            textBox5.Text = z.Tostring(z);
+        }
+    }
+}
